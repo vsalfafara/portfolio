d9a6f9b3954f6f60b50496d9bc11e55d7208804c
vsalfafara <alfafara.vm@gmail.com>
2022-07-09 14:36:45 +0800

    changed matthew to von

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
     <section className="hero" id="hero">
       <div className="container">
         <div className="hero-text">
-          <p className="blue">HELLO, I'M MATTHEW</p>
+          <p className="blue">HELLO, I'M VON</p>
           <h1 className="light">&lt; developer &gt;</h1>
           <p>Constantly grasping knowledge to this never ending technology evolution as it takes brain, skills, and enthusiasm to implement the best solution to a problem.</p>
           <button>GET STARTED</button>
